c981770b9ddd76011c6c30e4beef3cff862e56f0
KaranVikas <karanvikas15@gmail.com>
2025-10-03 11:44:05 -0400

    Add `UserRegisterationSerializer` with validation for username, email, and password-confirmation handling in `serializers.py`.

--- backend/todo/users/serializers.py
+++ backend/todo/users/serializers.py
@@ -10,3 +10,47 @@ class UserSerializer(serializers.ModelSerializer[User]):
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},
         }
+
+class UserRegisterationSerializer(serializers.ModelSerializer):
+  password = serializers.CharField(
+    write_only = True,
+    required = True,
+    validators=[validate_password],
+  )
+
+  password_confirm = serializers.CharField(
+    write_only = True,
+    required = True,
+  )
+
+  email = serializers.EmailField(required=True)
+
+  class Meta:
+    model = User
+    fields = ("username", "password","password_confirm","email","name")
+
+  def validate(self, attrs):
+    if attrs['password'] != attrs['password_confirm']:
+      raise serializers.ValidationError({
+        "password": "Password fields didn't match"
+      })
+    return attrs
+
+  def validate_email(self, value):
+    if User.objects.filter(email=value).exists():
+      raise serializers.ValidationError(
+        "A user with this email already exists."
+      )
+    return value
+
+  def validate_username(self, value):
+    if User.objects.filter(username=value).exists():
+      raise serializers.ValidationError(
+        "A user with this username already exists"
+      )
+    return value
+
+  def create(self, validated_data):
+    validated_data.pop('password_confirm', None)
+    user = User.objects.create_user(**validated_data)
+    return user
